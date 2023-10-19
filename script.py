22a7f26d568ab40b2fe6dcda471e56d4b9479f51
Godswill Umukoro <umukro6@gmail.com>
2023-10-19 16:29:23 +0100

    Introduction & Prompt Message & fallback alt for terminals that do not support ASCII Art

--- script.py
+++ script.py
@@ -2,6 +2,51 @@ import requests
 from bs4 import BeautifulSoup  # For parsing HTML
 import pandas as pd
 from io import StringIO
+from colorama import init, Fore, Style
+import pyfiglet
+
+# Initialize colorama
+init(autoreset=True)
+
+# ASCII art title
+title = r"""
+██████╗░██████╗░███████╗███╗░░░███╗██╗███████╗██████╗░  ██╗░░░░░███████╗░█████╗░░██████╗░██╗░░░██╗███████╗
+██╔══██╗██╔══██╗██╔════╝████╗░████║██║██╔════╝██╔══██╗  ██║░░░░░██╔════╝██╔══██╗██╔════╝░██║░░░██║██╔════╝
+██████╔╝██████╔╝█████╗░░██╔████╔██║██║█████╗░░██████╔╝  ██║░░░░░█████╗░░███████║██║░░██╗░██║░░░██║█████╗░░
+██╔═══╝░██╔══██╗██╔══╝░░██║╚██╔╝██║██║██╔══╝░░██╔══██╗  ██║░░░░░██╔══╝░░██╔══██║██║░░╚██╗██║░░░██║██╔══╝░░
+██║░░░░░██║░░██║███████╗██║░╚═╝░██║██║███████╗██║░░██║  ███████╗███████╗██║░░██║╚██████╔╝╚██████╔╝███████╗
+╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝╚═╝╚══════╝╚═╝░░╚═╝  ╚══════╝╚══════╝╚═╝░░╚═╝░╚═════╝░░╚═════╝░╚══════╝
+
+░██████╗░█████╗░██████╗░░█████╗░██████╗░███████╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝███████║██████╔╝█████╗░░██████╔╝
+░╚═══██╗██║░░██╗██╔══██╗██╔══██║██╔═══╝░██╔══╝░░██╔══██╗
+██████╔╝╚█████╔╝██║░░██║██║░░██║██║░░░░░███████╗██║░░██║
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚══════╝╚═╝░░╚═╝
+
+"""
+
+# Introduction & Prompt Message
+try:
+    print(Fore.GREEN + title)
+except:
+    print(pyfiglet.figlet_format("Premier League Scraper", font="slant"))
+
+print(Style.RESET_ALL + "Welcome to the " + Fore.GREEN +
+      "Premier League Scrapper" + Style.RESET_ALL + "!")
+print("Explore the latest scores and team information in the league.")
+
+# Instructions
+print("Use the following commands:")
+print("- " + Fore.YELLOW + "latest" +
+      Style.RESET_ALL + ": to see the latest score")
+print("- " + Fore.YELLOW + "top5" + Style.RESET_ALL + ": to see the top 5 teams")
+print("- " + Fore.YELLOW + "standings" + Style.RESET_ALL +
+      ": to view the current league standings")
+print("- " + Fore.YELLOW + "team <team_name>" + Style.RESET_ALL +
+      ": to get detailed information about a specific team")
+print("- " + Fore.YELLOW + "exit" + Style.RESET_ALL + ": to exit the application")
+
 
 stats_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
 
